e3733152994a143b8da7b05cd6d66d078be837cd
mick roper <mick@theropers.co.uk>
2024-02-17 16:41:18 +0000

    switched fmtter

--- model-railway-peripherals.ino
+++ model-railway-peripherals.ino
@@ -1,7 +1,8 @@
-void setup() {
-  Serial.begin(9600);
-}
+#include <Ethernet.h>
 
-void loop() {
-  
-}
+const byte mac[] = {0xDE, 0xAD, 0xBE, 0xEF, 0xFE, 0xED};
+const IPAddress ip(192, 168, 1, 177);
+
+void setup() { Serial.begin(9600); }
+
+void loop() {}
