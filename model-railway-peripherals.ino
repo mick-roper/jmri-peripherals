d9a2c70834f34cc08d776ae8d5023fd717ccb706
mick roper <mick@theropers.co.uk>
2024-02-17 18:39:05 +0000

    added some more serial output

--- model-railway-peripherals.ino
+++ model-railway-peripherals.ino
@@ -1,8 +1,46 @@
+#include <ArduinoMqttClient.h>
 #include <Ethernet.h>
 
-const byte mac[] = {0xDE, 0xAD, 0xBE, 0xEF, 0xFE, 0xED};
+byte mac[] = {0xDE, 0xAD, 0xBE, 0xEF, 0xFE, 0xED};
 const IPAddress ip(192, 168, 1, 177);
 
-void setup() { Serial.begin(9600); }
+EthernetClient ethernet;
+MqttClient mqttClient(ethernet);
 
-void loop() {}
+const char broker[] = "HW101075";
+int        port     = 1883;
+const char topic[]  = "track/*";
+
+const uint8_t minLoopDelay = 10;
+unsigned long lastUpdate;
+
+void setup() {
+  Serial.begin(9600);
+  while (!Serial) {};
+
+  Serial.print("Serial output initialised..");
+  Serial.println();
+
+  Ethernet.begin(mac, ip);
+  while (!ethernet.connected()) {
+    Serial.print("ethernet not connected...");
+    Serial.println();
+    delay(100);
+  }
+
+  if (!mqttClient.connect(broker, port)) {
+    Serial.print("MQTT connection failed...");
+    Serial.println();
+    while(1);
+  }
+
+  Serial.print("MQTT broker connection established!");
+  Serial.println();
+}
+
+void loop() {
+  if (millis() - lastUpdate >= minLoopDelay) {
+    lastUpdate = millis();
+    mqttClient.poll();
+  }
+}
